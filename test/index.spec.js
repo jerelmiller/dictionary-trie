d770e2b29912c125edc0d8150ef1d5244a13cbe7
Jerel Miller <jerelmiller@gmail.com>
2017-04-26 19:40:38 -0700

    Add rest of failing test cases

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,8 +2,22 @@ import createDictionary from '../src'
 
 describe('createDictionary', () => {
   it('returns searchable dictionary object', () => {
-    const dictionary = createDictionary(['test'])
+    const dictionary = createDictionary([])
 
     expect(dictionary).toHaveProperty('includes')
+  })
+
+  describe('#includes', () => {
+    it('returns true when word has been added', () => {
+      const dictionary = createDictionary(['test'])
+
+      expect(dictionary.includes('test')).toBe(true)
+    })
+
+    it('returns false when word has not been added', () => {
+      const dictionary = createDictionary(['blah'])
+
+      expect(dictionary.includes('test')).toBe(true)
+    })
   })
 })
