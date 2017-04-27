6a5e15dffb4b56723f9a30e95f6b4662c3dd827f
Jerel Miller <jerelmiller@gmail.com>
2017-04-26 23:18:00 -0700

    Move trie logic to dictionary

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-import createTrie from './trie'
+const TERMINATOR = '$'
 
 export default words => {
-  const trie = createTrie()
+  const root = {}
 
-  words.forEach(trie.insert)
+  const insert = word => {
+    let currentNode = root
+    characters(word).forEach(character => {
+      currentNode[character] = currentNode[character] || {}
+      currentNode = currentNode[character]
+    })
+    currentNode[TERMINATOR] = true
+  }
+
+  const characters = word => word.toLowerCase().split('')
+  const nodeFor = word =>
+    characters(word)
+      .reduce((node, character) => node[character] || {}, root)
+
+  const pathsFor = (node, str = '') =>
+    Object
+      .keys(node)
+      .map(character => {
+        if (character === TERMINATOR) {
+          return str
+        }
+
+        return pathsFor(node[character], str + character)
+      })
+      .reduce((x, y) => x.concat(y), [])
+
+  words.forEach(insert)
 
   return {
-    includes: trie.includes,
-    search: trie.search
+    includes: word => nodeFor(word)[TERMINATOR] || false,
+    search: word => pathsFor(nodeFor(word), word)
   }
 }
