f815d6f3decea32fb27e285aabb089e077ca09e0
Jerel Miller <jerelmiller@gmail.com>
2017-04-27 00:17:55 -0700

    More composition

--- src/index.js
+++ src/index.js
@@ -20,19 +20,24 @@ const dig = flip(prop)
 const flatten = reduce(concat, [])
 const set = curry((key, val, obj) => obj[key] = val)
 
-export default words => {
-  const root = {}
-
-  const insert = compose(
+const insertWith = root =>
+  compose(
     set(TERMINATOR, true),
     reduce((node, character) => node[character] = node[character] || {}, root),
     characters
   )
 
-  const nodeFor = compose(
+const digWith = root =>
+  compose(
     reduce(compose(or({}), dig), root),
     characters
   )
+
+export default words => {
+  const root = {}
+
+  const insert = insertWith(root)
+  const nodeFor = digWith(root)
 
   const pathsFor = (node, str = '') => {
     return compose(
